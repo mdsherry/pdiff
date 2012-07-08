643c28831d53da15887d78984d1841f9c215e535
Mark Sherry <mdsherry@gmail.com>
2012-07-08 17:34:38 -0400

    Seems to work

--- pdiff.hs
+++ pdiff.hs
@@ -3,6 +3,7 @@ import Data.HashMap as HashMap
 import Data.Hashable
 import Data.Text as T
 import Data.List as List
+import Debug.Trace
 
 uniqs :: (Ord a, Hashable a) => [a] -> Set a
 uniqs = uniqs' HashSet.empty
@@ -14,25 +15,20 @@ uniqs = uniqs' HashSet.empty
 				uniqs' (HashSet.insert x hash) xs
 		uniqs' hash _ = hash
 
+uniqMap :: (Ord a, Hashable a) => [a] -> Set a -> Map a Int
+uniqMap xs unique = HashMap.fromList $ List.filter (\(x,y) -> HashSet.member x unique) $ List.zip xs [1..]
 
-mapFilter :: ( a -> b -> (a,Maybe c) ) -> a -> [b] -> [c]
-mapFilter l seed (x:xs) = case (l seed x) of
-		(next, Just y) -> y : mapFilter l next xs
-		(next, Nothing) -> mapFilter l next xs							
-mapFilter _ _ _ = []
-
-uniqIndices xs unq = findIndices (\x -> HashSet.member x unq) xs
-
-uniqMap xs unique = HashMap.fromList $ List.filter (\(x,y) -> HashSet.member y unique) $ List.zip [1..] xs
 uniqCommon xs ys = HashSet.intersection (uniqs xs) (uniqs ys)
 
-blah :: [String] -> [String] -> Set String
-blah xs ys = common
+getPairs ::(Ord a, Hashable a) => [a] -> [a] -> [(Int, Int)]
+getPairs xs ys = deMaybe [(HashMap.lookup line uniqxmap, HashMap.lookup line uniqymap) | line <- HashSet.toList common ]
 	where
 		common = uniqCommon xs ys
-		--uniqxmap = uniqMap xs common
-		--uniqymap = uniqMap ys common
-
+		uniqxmap = uniqMap xs common
+		uniqymap = uniqMap ys common
+		deMaybe ((Just x, Just y):xs) = (x,y):deMaybe xs
+		deMaybe (_:xs) = deMaybe xs
+		deMaybe _ = []
 
 patience :: Ord a => [(a,a)] -> [(a, a)]
 patience xs = List.reverse $ List.last $ patience' xs []
@@ -41,11 +37,20 @@ patience xs = List.reverse $ List.last $ patience' xs []
 		patience' _ ls = ls
 		findLocation :: Ord a => (a,a) -> [[(a,a)]] -> [(a,a)] -> [[(a,a)]]
 		findLocation v [] past = [v:past]
-		findLocation v@(x, y) (l@((_,sy):history):ls) past = if y < sy 
+		findLocation v@(x, y) (l@((_,sy):_):ls) past = if y < sy 
 			then 
 				(v:past) : ls
 			else
 				l : findLocation v ls l
+
+processPairs ps@(p:_) xs ys = processPairs' (0,0) ps xs ys
+	where
+		processPairs' (xoff, yoff) (p@(px,py):ps) xs ys = (pdiff leadx leady) ++ (processPairs' (xoff + px, yoff + py) ps tailx taily)
+			where
+				(leadx, tailx) = List.splitAt (px - xoff) xs
+				(leady, taily) = List.splitAt (py - yoff) ys
+		processPairs' _ [] xs ys = pdiff xs ys
+processPairs [] xs ys = pdiff xs [] ++ pdiff [] ys
 
 
 matchStart :: Eq a => [a] -> [a] -> ([(Maybe a, Maybe a)], [a], [a])
@@ -57,13 +62,26 @@ matchStart (x:xs) (y:ys) = if x == y
 matchStart xs ys = ([], xs, ys)
 
 
+pdiff [] ys = [(Nothing, Just y) | y <- ys]
+pdiff xs [] = [(Just x, Nothing) | x <- xs]
 pdiff xs ys = startmatch ++ patienced ++ endmatch
 	where 
 		(startmatch, nonstartxs, nonstartys) = matchStart xs ys
 		(endmatch, midxs, midys) = (List.reverse ending, List.reverse xs, List.reverse ys) 
 			where 
 				(ending, xs, ys) = matchStart (List.reverse nonstartxs) (List.reverse nonstartys)
-		patienced = [] -- XXX midxs midys
-	
+		midpairs = List.sort $ getPairs midxs midys
+		patienced = processPairs midpairs midxs midys
+
+printable (Just x, Just y) = x
+printable (Just x, Nothing) = "-" ++ x
+printable (Nothing, Just y) = "+" ++ y	
+
 main = do
-		putStrLn "Hello world"
+		f1 <- readFile "pdiff.hs"
+		f2 <- readFile "pdiff2.hs"
+		let lines1 = List.lines f1
+		let lines2 = List.lines f2
+		let diff = pdiff lines1 lines2
+		mapM (putStrLn . printable) diff
+
