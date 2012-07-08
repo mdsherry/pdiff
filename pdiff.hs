a4bc0e810386262b0c681303b551c9facc8415d7
Mark Sherry <mdsherry@gmail.com>
2012-07-08 11:22:58 -0400

    Starting on actual patience algorithm

--- pdiff.hs
+++ pdiff.hs
@@ -1,7 +1,7 @@
 import Data.HashSet as HashSet
 import Data.HashMap as HashMap
 import Data.Hashable
-import Data.Text
+import Data.Text as T
 import Data.List as List
 
 uniqs :: (Ord a, Hashable a) => [a] -> Set a
@@ -25,3 +25,32 @@ uniqIndices xs unq = findIndices (\x -> HashSet.member x unq) xs
 
 uniqMap xs unique = HashMap.fromList $ List.filter (\(x,y) -> HashSet.member y unique) $ List.zip [1..] xs
 uniqCommon xs ys = HashSet.intersection (uniqs xs) (uniqs ys)
+
+blah xs ys =
+	where
+		common = uniqCommon xs ys
+		uniqxmap = uniqMap xs common
+		uniqymap = uniqMap ys common
+		pairs = 
+
+patience :: [a] -> [(Maybe a, Maybe a)]
+patience xs = patience' xs [] Nothing
+	where
+		patience' (x:xs) [] past = [(x, past)]
+		patience' (x:xs) (l:ls) past = if x < l 
+			then 
+				patience' xs 
+
+pdiff (x:xs) (y:ys) = startmatch ++ patienced ++ endmatch
+	where 
+		matchStart (x:xs) (y:ys) = if x == y 
+			then ((Just x, Just y) : remainder, remxs, remys)
+			else (remainder, remxs, remys)
+			where
+				(remainder, remxs, remys) = matchStart xs ys
+		(startmatch, nonstartxs, nonstartys) = matchStart xs ys
+		(endmatch, midxs, midys) = List.reverse $ matchStart (List.reverse nonstartxs) (List.reverse nonstartys)
+		patienced = XXX midxs midys
+	
+		
+
